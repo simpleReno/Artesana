845b9fbf7a31e5a33e2a3b98ae1fb9c74d88424c
simpleReno <ult.ofr.tato@gmail.com>
2024-08-06 14:56:30 -0600

    Set error

---
+++ pos_app/core/domain/models/error.py
@@ -0,0 +1,10 @@
+class Error():
+    def __init__(self, message):
+        self.message = message
+    
+    def __str__(self):
+        return self.message
+    
+    def __repr__(self):
+        return f'Error: {self.message}'
+    
