b5ac532c7e90914fa188b5cd1680c25d676ced14
simpleReno <ult.ofr.tato@gmail.com>
2024-08-15 17:53:45 -0600

    add category entity to database

---
+++ pos_app/adapters/database/models/catagory.py
@@ -0,0 +1,15 @@
+import datetime
+from sqlalchemy import Column, Integer, String, ForeignKey, Float, DateTime
+from sqlalchemy.orm import relationship
+from pos_app.adapters.database.sqlite_adapter.connection import Base, engine
+
+class CategoryEntity(Base):
+    __tablename__ = 'Categories'
+
+    id = Column(Integer, primary_key=True, unique=True)
+    products_id = Column(Integer, ForeignKey('products.id'))
+    product = relationship('Products', back_populates= 'categories')
+    description = Column(String)
+ 
+    
+Base.metadata.create_all(bind=engine)
