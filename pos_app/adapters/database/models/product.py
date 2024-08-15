8685c556af9bc94e9f3e857fdd90e5eb4e643ae4
simpleReno <ult.ofr.tato@gmail.com>
2024-08-15 17:54:43 -0600

    add product entity to database

--- pos_app/adapters/database/models/product.py
+++ pos_app/adapters/database/models/product.py
@@ -9,9 +9,10 @@ from pos_app.adapters.database.sqlite_adapter.connection import Base, engine
 class ProductEntity(Base):
     __tablename__ = 'Products'
 
-    product_id = Column(String, primary_key=True)
+    id = Column(Integer, primary_key=True, unique= True)
     name = Column(String)
     price = Column(Float)
+    description = Column(String)
     category_id = Column(Integer, ForeignKey('categories.id'))
     category = relationship('Category', back_populates='products')
     
