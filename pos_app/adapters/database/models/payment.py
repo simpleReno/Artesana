4e5716d9d28992c15ba95c958d1b747b5240cebf
simpleReno <ult.ofr.tato@gmail.com>
2024-08-15 17:54:29 -0600

    add payment entity to database

---
+++ pos_app/adapters/database/models/payment.py
@@ -0,0 +1,18 @@
+import datetime
+from sqlalchemy import Column, Integer, String, ForeignKey, Float, DateTime
+from sqlalchemy.orm import relationship
+from pos_app.adapters.database.sqlite_adapter.connection import Base, engine
+
+class PaymentEntity(Base):
+    __tablename__ = 'Payments'
+
+    id = Column(Integer, primary_key=True, unique=True)
+    payment_type = Column(String)
+    order_id = Column(Integer, ForeignKey('orders.id'))
+    order = relationship('Orders', back_populates= 'payments')
+    payment_amount = Column(Float, ForeignKey('Orders.total'))
+    amount = relationship('Orders', back_populates= 'payments')
+    description = Column(String)
+ 
+    
+Base.metadata.create_all(bind=engine)
