51235d20b115632a3d38e63d1bdbe2d5ba5d30d0
simpleReno <ult.ofr.tato@gmail.com>
2024-08-15 18:02:16 -0600

    Complete employee entity to database

--- pos_app/adapters/database/models/employee.py
+++ pos_app/adapters/database/models/employee.py
@@ -7,9 +7,10 @@ class EmployeeEntity(Base):
     __tablename__ = 'Employees'
 
     id = Column(Integer, primary_key=True, unique=True)
-    products_id = Column(Integer, ForeignKey('products.id'))
-    product = relationship('Products', back_populates= 'categories')
-    description = Column(String)
+    turn_id = Column(Integer, ForeignKey('turn.id'))
+    turn = relationship('Turns', back_populates= 'employees')
+    start_date = Column(DateTime)
+    Stop_date = Column(DateTime)
  
     
 Base.metadata.create_all(bind=engine)
