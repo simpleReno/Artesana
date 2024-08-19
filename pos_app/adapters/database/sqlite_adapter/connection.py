284bb8700bd6b4c84f6d6be5cc16553a1e388ecb
simpleReno <ult.ofr.tato@gmail.com>
2024-08-19 14:10:01 -0600

    Refactor Connection to db

--- pos_app/adapters/database/sqlite_adapter/connection.py
+++ pos_app/adapters/database/sqlite_adapter/connection.py
@@ -2,6 +2,7 @@ import os
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import text
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
@@ -13,13 +14,12 @@ load_dotenv()
 host = os.getenv('DB_HOST')
 user = os.getenv('DB_USER')
 database = os.getenv('DB_NAME')
-user = os.getenv('DB_USER')
 password = os.getenv('DB_PASSWORD')
-schema = os.getenv('DB_SCHEMA', 'public')
+port = os.getenv('DB_PORT')
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@{host}/{database}"
+SQLALCHEMY_DATABASE_URL = f'postgresql://{user}:{password}@{host}:{port}/{database}'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"options": f"-c search_path=dbo,{schema}"})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 Base = declarative_base()
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
