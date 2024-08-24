122965e54335288225402d5835ca4a1d3d45ddae
simpleReno <ult.ofr.tato@gmail.com>
2024-08-23 19:46:23 -0600

    refactor main

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import os
 from pos_app.adapters.ui.gui.main_app import Main_app
 from kivy.config import Config
-from pos_app.adapters.database.sqlite_adapter.connection import Connection
+from pos_app.adapters.database.psql_adapter.connection import engine, get_db
 
 def main():
     
-    DB = Connection.create_tables()
+    db = get_db()
+    
+    
+    
 
 if __name__ == '__main__':
     main()
