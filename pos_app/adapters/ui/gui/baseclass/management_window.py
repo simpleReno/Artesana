7facb0f62124d5456dfc2045234ba896e7be4f40
simpleReno <ult.ofr.tato@gmail.com>
2024-12-14 16:14:24 -0600

    management window has __init__

--- pos_app/adapters/ui/gui/baseclass/management_window.py
+++ pos_app/adapters/ui/gui/baseclass/management_window.py
@@ -2,7 +2,9 @@
 from kivymd.uix.screen import MDScreen
 
 class ManagementWindow(MDScreen):
-    
+    def __init__(self, **kwargs):
+          super().__init__(**kwargs)
+          
     def switch_navigation_rail(self, screen_name):
         self.ids.screen_manager.current = screen_name
 class ScreenTables(MDScreen):
