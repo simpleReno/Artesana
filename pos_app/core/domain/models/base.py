4cb9677de700c9449443cae37902ff5a2c2858fb
simpleReno <ult.ofr.tato@gmail.com>
2024-12-21 11:48:12 -0600

    delete to refactor al models

--- pos_app/core/domain/models/base.py
+++ pos_app/core/domain/models/base.py
@@ -1,4 +1,4 @@
 from sqlalchemy import Column, String, Table, Integer, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
